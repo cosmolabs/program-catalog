74f944bf15f301d2b5a7204fadfc9b01b0058f0e
Ganesh Kuramsetti <ganeshhkuramsetti@gmail.com>
2022-07-03 22:00:23 -0300

    Created a draft script that can take an input and download the video.

--- youtube-downloader/youtube-downloader.py
+++ youtube-downloader/youtube-downloader.py
@@ -5,14 +5,26 @@ try:
 except ModuleNotFoundError:
     raise ModuleNotFoundError("Module pytube is not installed yet ....") from None
 
-video = YouTube('https://www.youtube.com/watch?v=KGD2N5hJ2e0')
+url = input("Enter video url: ");
+video = YouTube(url)
+#video = YouTube('https://www.youtube.com/watch?v=5-OqPhet-NU')
 print('Process will start now ..........')
 print('Name of the video - ', video.title);
 video_streams = video.streams
-print("Available Streams are: ");
-for stream in video_streams:
-    print("Includes Audio - ", stream.includes_audio_track);
-    print("Includes Video - ", stream.includes_video_track);
-    print("Codecs -", stream.parse_codecs())
-    print("Resolution -", stream.res)
-    print("\n")
+#print(video_streams)
+print("Available Streams are: \n");
+for stream in video_streams.order_by('resolution').asc():
+    print(f"{stream.itag}|Audio - {stream.includes_audio_track}; Video - {stream.includes_video_track}; Codecs - {stream.parse_codecs()}; Resolution - {stream.resolution}; Type - {stream.mime_type}");
+
+itag_selected = input("Enter an itag from the above list: ");
+selected_stream = video_streams.get_by_itag(itag_selected)
+print("Includes Audio - ", selected_stream.includes_audio_track);
+print("Includes Video - ", selected_stream.includes_video_track);
+print("Codecs -", selected_stream.parse_codecs())
+print("Resolution -", selected_stream.resolution)
+print("itag -", selected_stream.itag)
+print("Type -", selected_stream.mime_type)
+print(f"File Size - {round(selected_stream.filesize*0.000001, 2)} MB");
+print("Downloading the selected stream .......")
+selected_stream.download()
+print("Download Completed .....")
