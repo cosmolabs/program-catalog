2c6d5df819971785415cc1b3c4877959971ebee8
Ganesh Kuramsetti <ganeshhkuramsetti@gmail.com>
2022-07-04 21:02:18 -0300

    Modified the ytvd script.

--- youtube-downloader/youtube-downloader.py
+++ youtube-downloader/youtube-downloader.py
@@ -5,26 +5,42 @@ try:
 except ModuleNotFoundError:
     raise ModuleNotFoundError("Module pytube is not installed yet ....") from None
 
-url = input("Enter video url: ");
-video = YouTube(url)
-#video = YouTube('https://www.youtube.com/watch?v=5-OqPhet-NU')
-print('Process will start now ..........')
-print('Name of the video - ', video.title);
-video_streams = video.streams
-#print(video_streams)
-print("Available Streams are: \n");
-for stream in video_streams.order_by('resolution').asc():
-    print(f"{stream.itag}|Audio - {stream.includes_audio_track}; Video - {stream.includes_video_track}; Codecs - {stream.parse_codecs()}; Resolution - {stream.resolution}; Type - {stream.mime_type}");
+print("\nWelcome to YTVd!!!\n")
 
-itag_selected = input("Enter an itag from the above list: ");
-selected_stream = video_streams.get_by_itag(itag_selected)
+print("Enter the url of the video you want to download.");
+video_url = input("Video URL: ");
+video = YouTube(video_url)
+
+print('\nTitle of the video - ', video.title);
+
+print("\nEnter A for only Audio, V for only video, AV for both Audio and Video.")
+user_AV_selection = input("A or V or AV: ")
+
+audio_streams = video.streams.filter(only_audio=True)
+video_streams = video.streams.filter(only_video=True)
+downloadable_streams = [];
+
+if user_AV_selection == "A":
+    downloadable_streams = audio_streams;
+if user_AV_selection == "V":
+    downloadable_streams = video_streams.order_by('resolution').asc();
+if user_AV_selection == "AV":
+    downloadable_Streams = "";
+
+for a_stream in downloadable_streams:
+    print(f"{a_stream.itag} | Codecs(Video, Audio) - {a_stream.parse_codecs()}; Resolution - {a_stream.resolution}; Type - {a_stream.mime_type}; File Size - {round(a_stream.filesize*0.000001, 2)} MB");
+
+itag_selected = input("\nEnter the record number from the above list: ");
+
+selected_stream = downloadable_streams.get_by_itag(itag_selected)
+
+print("\nDetails of selected record: ")
 print("Includes Audio - ", selected_stream.includes_audio_track);
 print("Includes Video - ", selected_stream.includes_video_track);
-print("Codecs -", selected_stream.parse_codecs())
 print("Resolution -", selected_stream.resolution)
-print("itag -", selected_stream.itag)
 print("Type -", selected_stream.mime_type)
 print(f"File Size - {round(selected_stream.filesize*0.000001, 2)} MB");
-print("Downloading the selected stream .......")
+
+print("\nDownload started !!!")
 selected_stream.download()
-print("Download Completed .....")
+print("\nDownload complete !!!")
